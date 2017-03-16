685585ac649337a2bc5d1a3debf4ea25d53659d2
narghev <n.ghevandiani@gmail.com>
2017-03-17 00:43:38 +0400

    countries initial rendering

--- lib/App.jsx
+++ lib/App.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Countries from '../countries.js';
 
 class App extends React.Component {
-   render() {
-      return (
-        <p>Hello</p>
-      );
-   }
+  constructor(){
+    super();
+    this.state = {
+      list: Countries
+    }
+    this.content = new Array();
+  }
+  render(){
+    this.content = [];
+    for (let i=0; i < this.state.list.length; i++) {
+      this.content.push(
+        <div className="country" key={ i }>
+          <span>
+            {
+              this.state.list[i].name
+            }
+          </span>
+        </div>
+      )
+    }
+    return (
+      <div className="list">
+        {
+          this.content
+        }
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
